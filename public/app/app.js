87f17d5b1f3a63ccb511dbeae751f887947ef466
pmarchand122 <pmarchan@iu.edu>
2021-12-05 22:22:19 -0500

    todo: cart functionality and media queries

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,11 @@
 function route() {
   let pageID = window.location.hash;
   let ID = pageID.replace("#/", "");
-  if (ID == "") {
-    MODEL.getPageContent("main");
+  if (ID == "" || ID == "home") {
+    MODEL.getPageContent("home");
+    loadRecipes();
+  } else if (ID == "cart") {
+    MODEL.getPageContent("cart");
   } else {
     MODEL.getPageContent(ID);
   }
@@ -13,11 +16,129 @@ function initListeners() {
   route();
 }
 
+function initFirebase() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      $("#login").html(
+        `<i id="logoutButton" onclick='logout()' class='fas fa-sign-out-alt'></i>`
+      );
+      console.log("There is a user");
+    } else {
+      $("#login").html(`<img src='./images/login.svg' />`);
+      console.log("This is no user");
+    }
+  });
+}
+
+function logout() {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      // Sign-out successful.
+      window.alert("You have signed out");
+    })
+    .catch((error) => {
+      console.log(error);
+      window.alert(error);
+    });
+}
+
+function login() {
+  console.log("login");
+  let password = $("#loginPassword").val();
+  let email = $("#loginEmail").val();
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      var user = userCredential.user;
+      window.alert("Signed In");
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert(errorMessage);
+    });
+}
+
+//cart array
+let cartArray = [];
+function addtoCart(id) {
+  console.log(id);
+  window.alert("You have added to your cart");
+  cartArray.push(id);
+}
+
+function loadRecipes() {
+  $.getJSON("data/coffeeMakers.json", function (data) {
+    $.each(data, function (index, coffeeMaker) {
+      $(
+        ".coffeeMakers"
+      ).append(`<div class="coffeeMaker" id="coffeeMaker${coffeeMaker.id}">
+      <div class="imgHolder">
+        <img src="../../images/coffeeMaker${coffeeMaker.id}.webp" />
+      </div>
+      <div class="colorPicker">
+        <div class="largeCircle">
+          <div class="whiteCircle">
+            <div class="smallCircle"></div>
+          </div>
+        </div>
+      </div>
+      <div class="coffeeInfo">
+        <h1>${coffeeMaker.name}</h1>
+        <p>$${coffeeMaker.price}</p>
+        <div class="ratings">
+          <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="far fa-star"></i>
+          </div>  
+          <p>${coffeeMaker.rating.decimal} | (${coffeeMaker.rating.number})</p>
+        </div>
+        <div class="freeShipping">
+          <img src="../../images/shipping.svg"/>
+          <p>Free shipping</p>
+        </div>
+        <div class="buyNow" onclick="addtoCart(${coffeeMaker.id})">
+          <h2>BUY NOW</h2>
+        </div>
+      </div>
+    </div>`);
+    });
+  });
+}
+
+function signup() {
+  console.log("signup");
+  let password = $("#signupPassword").val();
+  let email = $("#signupEmail").val();
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      var user = userCredential.user;
+      console.log(user);
+      window.alert("User has signed up!");
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+    });
+}
+
 $(document).ready(function () {
   initListeners();
+
   try {
     let app = firebase.app();
-    //initFirebase();
+    initFirebase();
   } catch {
     console.log(e);
     console.log(errorMessage);
