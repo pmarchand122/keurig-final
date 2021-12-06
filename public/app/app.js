477769da5590ef8f5394c427d5f89f4cdf4d3056
pmarchand122 <pmarchan@iu.edu>
2021-12-06 15:03:16 -0500

    almost done

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,12 @@
 function route() {
   let pageID = window.location.hash;
   let ID = pageID.replace("#/", "");
-  if (ID == "" || ID == "home") {
-    MODEL.getPageContent("home");
-    loadRecipes();
-  } else if (ID == "cart") {
-    MODEL.getPageContent("cart");
+  if (ID == "" || ID == "main") {
+    MODEL.getPageContent("main");
+    loadCoffeeMakers();
+    coffeeMakers = [];
+  } else if (ID == "checkout") {
+    MODEL.getPageContent("checkout", appendToCart);
   } else {
     MODEL.getPageContent(ID);
   }
@@ -63,19 +64,88 @@ function login() {
     });
 }
 
-//cart array
+//cart array with ID's
 let cartArray = [];
+
+//array with objects that match cartArray ID's
+let coffeeArray = [];
+
 function addtoCart(id) {
   console.log(id);
   window.alert("You have added to your cart");
   cartArray.push(id);
 }
 
-function loadRecipes() {
+function appendToCart() {
+  console.log(cartArray);
+  for (var i = 0; i < coffeeMakers.length; i++) {
+    for (var x = 0; x < cartArray.length; x++) {
+      if (coffeeMakers[i].id === cartArray[x]) {
+        coffeeArray.push(coffeeMakers[i]);
+      }
+    }
+  }
+
+  console.log(coffeeArray);
+  for (var i = 0; i < coffeeArray.length; i++) {
+    $("#cartItems").append(`
+    <div class="coffeeMaker" id="cartItem${coffeeArray[i].id}">
+    <div class="imgHolder">
+      <img src="../../images/coffeeMaker${coffeeArray[i].id}.webp" />
+    </div>
+    <div class="colorPicker">
+      <div class="largeCircle">
+        <div class="whiteCircle">
+          <div class="smallCircle"></div>
+        </div>
+      </div>
+    </div>
+    <div class="coffeeInfo">
+      <h1>${coffeeArray[i].name}</h1>
+      <p>$${coffeeArray[i].price}</p>
+      <div class="ratings">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="far fa-star"></i>
+        </div>  
+        <p>${coffeeArray[i].rating.decimal} | (${coffeeArray[i].rating.number})</p>
+      </div>
+      <div class="freeShipping">
+        <img src="../../images/shipping.svg"/>
+        <p>Free shipping</p>
+      </div>
+      <div class="removeFromCart" onclick="removeFromCart(${coffeeArray[i].id})">
+        <h2>REMOVE FROM CART</h2>
+      </div>
+    </div>
+  </div>
+    `);
+  }
+
+  coffeeArray = [];
+}
+
+function removeFromCart(ID) {
+  console.log("REMOVE");
+  for (var i = 0; i < coffeeArray.length; i++) {
+    if (coffeeArray[i].id == ID) {
+      coffeeArray.splice(i);
+    }
+    console.log(coffeeArray);
+  }
+}
+
+//array that holds json data;
+let coffeeMakers = [];
+function loadCoffeeMakers() {
   $.getJSON("data/coffeeMakers.json", function (data) {
     $.each(data, function (index, coffeeMaker) {
+      coffeeMakers.push(coffeeMaker);
       $(
-        ".coffeeMakers"
+        "#homeCoffeeMakers"
       ).append(`<div class="coffeeMaker" id="coffeeMaker${coffeeMaker.id}">
       <div class="imgHolder">
         <img src="../../images/coffeeMaker${coffeeMaker.id}.webp" />
@@ -111,6 +181,8 @@ function loadRecipes() {
     </div>`);
     });
   });
+
+  console.log(coffeeMakers);
 }
 
 function signup() {
